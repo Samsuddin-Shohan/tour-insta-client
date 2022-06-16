f28af31e00bd3f01a9d80f67b1dd563efe307183
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-06-16 19:50:57 +0600

    firebase configured

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: process.env.REACT_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: REACT_FIREBASE_MESSAGINGSENDERID,
+    appId: REACT_FIREBASE_APPID
+};
+
+export default firebaseConfig;
+  
